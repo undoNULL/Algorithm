409bb65b5628702f7e03334f0329e5ca7d6fe22b
undoNULL <HS.Kang.Dev@gmail.com>
2022-05-06 18:00:45 +0900

    bfs implementation need greater for priority queue

--- Baekjoon_3197/Baekjoon_3197/main.cpp
+++ Baekjoon_3197/Baekjoon_3197/main.cpp
@@ -2,6 +2,8 @@
 #include<queue>
 using namespace std;
 
+#define MIN(val, min) (((val)>(min))?(val):(min))
+
 int map[1501][1501] = { 0, };
 int visit[1501][1501] = { 0, };
 
@@ -14,7 +16,15 @@ void debug_printMap(int w, int h) {
 		cout << endl;
 	}
 }
-
+void debug_printVisit(int w, int h) {
+	cout << endl;
+	for (int i = 0;i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			cout << visit[i][j];
+		}
+		cout << endl;
+	}
+}
 
 int conv(char c) {
 	switch (c) {
@@ -86,9 +96,36 @@ void setMapIceMeltTime(int w, int h) {
 	}
 }
 
-void bfs(pair<int, int> start, pair<int, int> end) {
-
+void bfs(pair<int, int> start, pair<int, int> end, int w, int h) {
+	int xx[4] = { 1, 0 ,-1, 0 };
+	int yy[4] = { 0,-1,0,1 };
+	priority_queue<pair<int, pair<int, int>>> pq; //greater ÇÊ¿ä
+	//queue<pair<int, int>> pq;
+	pair<int, int> cur;
+	int x = start.first, y = start.second, nX, nY;
+	visit[y][x] = 1;
+	pq.push({ visit[y][x], { x, y } });
+	while (!pq.empty()) {
+		cur = pq.top();
+		x = cur.first;
+		y = cur.second;
+		pq.pop();
+		if (end.first == x && end.second == y)
+			break;
+		for (int i = 0; i < 4; i++) {
+			nX = x + xx[i];
+			nY = y + yy[i];
+			if (inRange(nX, 0, w) && inRange(nY, 0, h)) {
+				if (visit[nY][nX] != 0)
+					continue;
+				//visit[nY][nX] = visit[y][x]; //map[nY][nX]+1;
+				visit[nY][nX] = visit[y][x] + MIN(map[nY][nX]+1 - visit[y][x], 0);
+				pq.push({visit[nY][nX], { nX,nY } });
+			}
+		}
+	}
 }
+
 
 int main() {
 	int w, h;
@@ -97,8 +134,10 @@ int main() {
 	getMap(w, h, swan);
 
 	setMapIceMeltTime(w, h);
+	bfs(swan[0], swan[1], w, h);
 
 	debug_printMap(w, h);
+	cout << endl << "Visit";
+	debug_printVisit(w, h);
 
-	
 }
